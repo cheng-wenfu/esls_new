d538c5aee126cdfda190d86c787c6bed092f0011
cwf <chengwenfu00@gmail.com>
2020-04-03 23:05:28 +0800

    添加数据请求接口

--- src/services/tags.ts
+++ src/services/tags.ts
@@ -7,14 +7,13 @@ interface TagsDataRequestParams extends RequestParams {
 }
 
 /**
- *根据条件获取商品信息
+ *根据条件获取价签信息
  *
  * @param query string 查询条件 可为所有字段 分隔符为单个空格
  * @param queryString string | number 查询条件的字符串
  * @param page number  页码
  * @param count number 数量
  */
-
 export async function getTagsData({page, query = "", queryString = "",  count = 10}: TagsDataRequestParams) {
 
   const url = `/server/api/tags/?query=${query}&queryString=${queryString}&page=${page}&count=${count}`;
@@ -23,4 +22,25 @@ export async function getTagsData({page, query = "", queryString = "",  count = 
     method: 'GET',
   })
 }
+
+/**
+ * 获取价签统计数据
+ */
+export async function getTagStatisticData() {
+  return request('/server/api/tags/index', {
+    method: 'GET'
+  })
+}
+
+/**
+ * 获取变价超时的价签数据
+ * @param page
+ */
+export async function getOvertimeTagsData(page: number) {
+  const url = `/api/tags/overtime?page=${page}$count=10`;
+  return request(url, {
+    method: 'GET',
+  })
+}
+
 
