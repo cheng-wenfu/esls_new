edd2aa17da567279ea388b3dad70c703229b635d
cwf <chengwenfu00@gmail.com>
2020-04-03 23:03:37 +0800

    主页子组件

--- src/pages/Home/components/HomeStatisticDataPie.tsx
+++ src/pages/Home/components/HomeStatisticDataPie.tsx
@@ -1,1 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { Pie } from '@antv/g2plot';
 
+
+interface valueType {
+  type: string;
+  value: number;
+}
+
+interface HomeStatisticDataPieProps {
+  value: valueType[];
+  elementId: string;
+  title?: string;
+  description?: string;
+}
+
+const HomeStatisticDataPie: React.FC<HomeStatisticDataPieProps>  = ({value, elementId, title, description}) => {
+  // const [pie, setPie] = useState<Pie>()
+  useEffect(() => {
+    const piePlot = new Pie(document.getElementById(elementId), {
+      forceFit: true,
+      title: {
+        visible: true,
+        text: title || "饼图",
+      },
+      description: {
+        visible: true,
+        text: description || "这是一个饼图"
+      },
+      radius: 0.8,
+      data: value,
+      angleField: 'value',
+      colorField: 'type',
+      label: {
+        visible: true,
+        type: 'inner',
+      }
+    });
+    // setPie(piePlot);
+    // console.log("piePlot", piePlot)
+    piePlot.render()
+  },[])
+    // const piePlot = new Pie(document.getElementById(elementId), {
+    //   forceFit: true,
+    //   title: {
+    //     visible: true,
+    //     text: title || "饼图",
+    //   },
+    //   description: {
+    //     visible: true,
+    //     text: description || "这是一个饼图"
+    //   },
+    //   radius: 0.8,
+    //   data: value,
+    //   angleField: 'value',
+    //   colorField: 'type',
+    //   label: {
+    //     visible: true,
+    //     type: 'inner',
+    //   }
+    // });
+    // piePlot.render()
+
+    // useEffect(() => {
+    //   console.log("pie", pie)
+    //   //piePlot.changeData(value)
+    // },[])
+
+  return <div id={elementId}></div>
+}
+
+export default HomeStatisticDataPie;
