6cfb0b1aad3a842df9f3f293fc6f5d30ff6ffa7d
cwf <chengwenfu00@gmail.com>
2020-04-03 23:04:34 +0800

    主页添加子组件

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,8 +7,8 @@ import { HomeOutlined } from '@ant-design/icons'
 
 
 import HomeStatisticDataCard from './components/HomeStatisticDataCard';
-import OperationLogsTable from './components/OperationLogsTable'
-
+import OperationLogsTable from './components/OperationLogsTable';
+import HomeStatisticDataPie from './components/HomeStatisticDataPie';
 
 interface HomeProps {
   dispatch: Dispatch;
@@ -17,6 +17,51 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = (props) => {
   const { user, dispatch } = props;
+
+  const { normalTagSize, noBindTagSize, noIsWorkingTagSize, forbiddenTagSize } = user.homeStatisticData;
+  const tagData = {
+    value: [
+      {
+        type: '正常',
+        value: normalTagSize,
+      },
+      {
+        type: '未绑定',
+        value: noBindTagSize,
+      },
+      {
+        type: '离线',
+        value: noIsWorkingTagSize,
+      },
+      {
+        type: '禁用',
+        value: forbiddenTagSize,
+      },
+    ],
+    title: '价签信息',
+    elementId: 'canvas1',
+    description: '所有价签的统计信息'
+  };
+  const { normalRouterSize, forbiddenRouterSize, noIsWorkingRouterSize } = user.homeStatisticData;
+      const routerData = {
+        value: [
+          {
+            type: '正常',
+            value: normalRouterSize,
+          },
+          {
+            type: '离线',
+            value: noIsWorkingRouterSize,
+          },
+          {
+            type: '禁用',
+            value: forbiddenRouterSize,
+          },
+        ],
+        title: '路由器信息',
+        elementId: 'canvas2',
+        description: '所有路由器的统计信息'
+      };
 
   useEffect(() => {
     console.log(user)
@@ -67,6 +112,14 @@ const Home: React.FC<HomeProps> = (props) => {
             icon={<HomeOutlined />}
             title="用户数量"
             value={user.homeStatisticData.userSize} />
+        </Col>
+      </Row>
+      <Row gutter={[8,24]}>
+        <Col span={12}>
+          <HomeStatisticDataPie {...tagData} />
+        </Col>
+        <Col span={12}>
+          <HomeStatisticDataPie {...routerData} />
         </Col>
       </Row>
       <Row gutter={8}>
