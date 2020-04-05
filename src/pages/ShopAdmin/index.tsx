46120733fa64aaf3a17219298088b02746d7c5b7
cwf <chengwenfu00@gmail.com>
2020-04-05 21:40:33 +0800

    店铺管理入口文件

---
+++ src/pages/ShopAdmin/index.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import { connect, Dispatch } from 'umi';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Row, Card, Table } from 'antd';
+import { ConnectState, ShopsModelState } from '@/models/connect';
+
+import ShopTable from './components/ShopTable'
+
+
+
+interface ShopAdminProps {
+  dispatch: Dispatch;
+  shops: ShopsModelState;
+}
+
+
+const ShopAdmin: React.FC<ShopAdminProps> = (props) => {
+  const { dispatch, shops } = props;
+  useEffect(() => {
+    dispatch({
+      type: 'shops/getShopsData',
+      payload: {
+        page: 0,
+      }
+    })
+    dispatch({
+      type: 'shops/getHeadShopData',
+    })
+  }, []);
+
+  const columns = [
+    {
+      title: '店铺编码',
+      dataIndex: 'number'
+    },
+    {
+      title: '店铺名称',
+      dataIndex: 'name',
+    },
+    {
+      title: '店长姓名',
+      dataIndex: 'manager',
+    },
+    {
+      title: '地址',
+      dataIndex: 'address',
+    },
+    {
+      title: '电话',
+      dataIndex: 'phone'
+    },
+    {
+      title: '路由器',  //TODO: 索引错误
+      dataIndex: 'type',
+    }
+  ]
+
+  return (
+    <PageHeaderWrapper>
+      <Row gutter={[8, 24]}>
+        <Card title="总店信息">
+          <Table dataSource={shops.headShopData} columns={columns} />
+        </Card>
+      </Row>
+      <Row gutter={8}>
+        <ShopTable dispatch={dispatch} shopsData={shops.shopsData} />
+      </Row>
+    </PageHeaderWrapper>
+  )
+}
+
+export default connect(({ shops }: ConnectState) => ({
+  shops: shops
+}))(ShopAdmin);
